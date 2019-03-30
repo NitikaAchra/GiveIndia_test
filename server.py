848bb8c3414d554216a6c35660363e52debef43e
Nitika Achra <nachra@QLI-MAC-008.local>
2019-03-31 03:17:56 +0530

    This checkin includes-  1) Delete method  2) Query field in response  3) Endpoint /process/*  4) Error check for invalid path

--- server.py
+++ server.py
@@ -1,9 +1,9 @@
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
-import json as simplejson
+import json
 import datetime
 import time
-from collections import namedtuple
-pair = namedtuple('pair', ["first", "second"])
+from urlparse import urlparse
+import re
 
 class Node:
     def __init__(self, dataval=None):
@@ -41,18 +41,27 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
         else:
             self.request_list.tail.next = request_node
             self.request_list.tail = self.request_list.tail.next
+    
+    def toJSON(self,response):
+        return json.dumps(response,default=lambda x: getattr(x, '__dict__', str(x)),
+            sort_keys=True, indent=4)
 
 
     def create_response(self,header,body,path,method):
-        if(path == '/process'):
-            response = {"timestamp": str(datetime.datetime.now()), "method": method, "path": path, "header": header, "body": body, "duration": 15}
-        elif(path == '/stats'):
-            response = self.get_stats()
+        response = {}
+        if(re.search("^/process*", path)):
+            query = urlparse(self.path).query
+            query_components = {}
+            if(query):
+                query_components = dict(qc.split("=") for qc in query.split("&"))
+            response = {"timestamp": str(datetime.datetime.now()), "method": method, "path": path, "header": header, "body": body, "query": query_components, "duration": 15}
+        elif(re.search("^/stats$", path)):
+            response = self.get_stats()   
         return response
 
     def get_stats(self):
         result = {"Total":{},"Last_hour":{},"Last_minute":{}}
-        requests = ["GET","POST","PUT"]
+        requests = ["GET","POST","PUT","DELETE"]
         now = time.time()
         list_current = self.request_list.head
         list_prev = None
@@ -63,7 +72,6 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
             request_arrival = current_request["arrival_time"]
             request_type = current_request["type"]
             request_responsetime = current_request["response_time"]
-            print now, request_arrival
             if ((now - request_arrival) > 3600) :
                 self.last_hour[request_type]["total"] -= 1
                 self.last_hour[request_type]["response_time"] -= request_responsetime
@@ -72,7 +80,6 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
                 else:
                     self.request_list.head = next_request
             if((now - request_arrival) > 60):
-                print "Last minute"
                 self.last_minute[request_type]["total"] -= 1
                 self.last_minute[request_type]["response_time"] -= request_responsetime
             list_prev = list_current
@@ -105,11 +112,16 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
         start = time.time()
         time.sleep(15)
         header = self.headers
-        self.send_response(200)
-        self.end_headers()
         path = self.path
-        response_data = self.create_response(header,"",path,"GET")
-        self.wfile.write(response_data)
+        response_data = self.create_response(header,"",self.path,"GET")
+        if  not response_data:
+            self.send_response(400)
+            self.end_headers()
+            self.wfile.write("PATH NOT FOUND")
+        else:
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(self.toJSON(response_data))
         response_time = time.time() - start
         self.total["GET"]["response_time"] += response_time
         self.last_hour["GET"]["response_time"] += response_time
@@ -131,10 +143,15 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
         content_length = int(self.headers['Content-Length'])
         header = self.headers
         body = self.rfile.read(content_length)
-        self.send_response(200)
-        self.end_headers()
         response_data = self.create_response(header,body,self.path,"POST")
-        self.wfile.write(response_data)
+        if  not response_data:
+            self.send_response(400)
+            self.end_headers()
+            self.wfile.write("PATH NOT FOUND")
+        else:
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(self.toJSON(response_data))
         response_time = time.time() - start
         self.total["POST"]["response_time"] += response_time
         self.last_hour["POST"]["response_time"] += response_time
@@ -156,16 +173,51 @@ class HTTPRequestHandler(BaseHTTPRequestHandler):
         content_length = int(self.headers['Content-Length'])
         header = self.headers
         body = self.rfile.read(content_length)
-        self.send_response(200)
-        self.end_headers()
         response_data = self.create_response(header,body,self.path,"PUT")
-        self.wfile.write(response_data)
+        if  not response_data:
+            self.send_response(400)
+            self.end_headers()
+            self.wfile.write("PATH NOT FOUND")
+        else:
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(self.toJSON(response_data))
         response_time = time.time() - start
         self.total["PUT"]["response_time"] += response_time
         self.last_hour["PUT"]["response_time"] += response_time
         self.last_minute["PUT"]["response_time"] += response_time
         this_request =	{
                     "type": "PUT",
+                    "response_time": response_time,
+                    "arrival_time": start
+                    }
+        self.insert_node(this_request)
+
+    def do_DELETE(self):
+        self.total["DELETE"]["total"] += 1
+        self.last_hour["DELETE"]["total"] += 1
+        self.last_minute["DELETE"]["total"] += 1
+        now = datetime.datetime.now()
+        start = time.time()
+        time.sleep(15)
+        content_length = int(self.headers['Content-Length'])
+        header = self.headers
+        body = self.rfile.read(content_length)
+        response_data = self.create_response(header,body,self.path,"DELETE")
+        if  not response_data:
+            self.send_response(400)
+            self.end_headers()
+            self.wfile.write("PATH NOT FOUND")
+        else:
+            self.send_response(200)
+            self.end_headers()
+            self.wfile.write(self.toJSON(response_data))
+        response_time = time.time() - start
+        self.total["DELETE"]["response_time"] += response_time
+        self.last_hour["DELETE"]["response_time"] += response_time
+        self.last_minute["DELETE"]["response_time"] += response_time
+        this_request =	{
+                    "type": "DELETE",
                     "response_time": response_time,
                     "arrival_time": start
                     }
